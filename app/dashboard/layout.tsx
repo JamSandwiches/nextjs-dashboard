4b63d91a8d472b4365c2d8694f968ccbacad818b
Michael Bartlett <Michael.Bartlett@telus.com>
2024-01-30 14:48:59 -0800

    Update dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,13 +3,12 @@ import { inter } from '@/app/ui/fonts';
 
 export default function Layout({ children }: { children: React.ReactNode; }) {
     return (
-        <html lang="en">
-            <div className="flex h-screen flex-col md:flex-row md:overflow-hidde">
-                <div className="w-full flex-none md:w-64">
-                    <SideNav />
-                </div>
-                <div className={`${inter.className} antialiased flex-grow p-6 md:overflow-y-auto md:p-12`}>{children}</div>
+        <div className="flex h-screen flex-col bg-slate-500 md:flex-row md:overflow-hidde">
+            <div className="w-full flex-none md:w-64">
+                <SideNav />
             </div>
-        </html>
+            {/* <div className={`${inter.className} antialiased flex-grow p-6 md:overflow-y-auto md:p-12`}>{children}</div> */}
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
     );
 }
